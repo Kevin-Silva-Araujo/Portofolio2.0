67084c83ee3a63dd9277ddbed9fea9bf5f451cc6
Kevin Silva Araujo <123kevindante@gmail.com>
2023-12-14 17:46:19 -0300

    Update de alguns bugs e add de novas animações

--- js/main.js
+++ js/main.js
@@ -16,7 +16,61 @@ chk.addEventListener('change',()=>{
 });
 
 
+//pega o preloader pelo DOM
+const preloader = document.querySelector(".preloader");
+//espera a pagina carregar totalmente
+document.addEventListener("DOMContentLoaded",()=>{
 
+setTimeout(()=>{
+    gsap.to(".preloader",
+    {
+        opacity:0,
+        duration:2,
+        x:200,
+        //once the preloader has faded out
+        onComplete(){
+            //remove rotationg css animation
+            preloader.computedStyleMap.animation = "none";
+            //hide the preloader
+            preloader.computedStyleMap.display = "none"
+
+        },
+    },
+    
+    );
+},50)
+
+gsap.fromTo("#header",{opacity:0},
+{
+    opacity:1,
+    ease:"power3.out",
+    duration:1.5,
+    delay:1,
+    y:50
+    
+})
+gsap.fromTo("#Me",
+{opacity:0},
+{
+    opacity:1,
+    ease:"power3.out",
+    duration:1.5,
+    delay:2
+});
+gsap.fromTo(".text",
+{opacity:0},
+{
+    opacity:1,
+    ease:"power3.out",
+    duration:1.5,
+    delay:2,
+});
+gsap.fromTo("#icone",
+{opacity:0,y:10,},
+{opacity:1,y:0,ease: "power3.out", duration:2,delay:3}
+
+);
+});
    
     
 
